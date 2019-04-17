d564eea6f06a2a418e265be6287b5d6c4d999d49
alexander1212121212 <49705564+alexander1212121212@users.noreply.github.com>
2019-04-16 23:56:41 -0400

    Add files via upload

---
+++ Operaciones.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using Entidades;
+
+namespace Operador
+{
+    public class Operacoes : Entidade
+    {
+        public void Operacao(string oper)
+        {
+            operacao = oper;
+        }
+        //public bool  ContainsOperacao => operacao.Any();
+
+        public double? RealizarOperacao(double n2)
+        {
+            if (operacao == "")
+                n1 = n2;
+            else 
+                switch (operacao)
+                {
+                    case "+":
+                        n1 += n2;
+                        break;
+
+                    case "-":
+                        n1 -= n2;
+                        break;
+
+                    case "*":
+                        n1 *= n2;
+                        break;
+
+                    case "/":
+                        n1 /= n2;
+                        break;
+                }
+            return n1;
+        }
+
+        public void Limpar()
+        {
+            n1 = 0;
+            operacao = "";
+        }
+    }
+}
